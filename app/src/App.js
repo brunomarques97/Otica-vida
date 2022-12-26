57559e2e1f5cb36a2b2cf84364834109cbdc31f9
brunomarques97 <116980642+brunomarques97@users.noreply.github.com>
2022-12-26 16:53:22 -0300

    Montar cabeçalho

--- app/src/App.js
+++ app/src/App.js
@@ -1,8 +1,18 @@
 import './App.css';
-
+//imagens
+import logo from './imagens/logo.png';
 function App() {
+  
   return (
     <div>
+      <div className="cabecalho" >
+        <img src={logo} alt="LOGO"/>
+        <div className="links">
+            <a>PRODUTOS</a>
+            <a>SOBRE</a>
+            <a>CONTATO</a>
+        </div>
+      </div>
 
     </div>
   );
